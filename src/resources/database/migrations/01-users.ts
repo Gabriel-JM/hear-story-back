6b80363dfc0db1f826ed69fa4e4622f6950691e9
Gabriel-JM <gabriel.josemelo@hotmail.com>
2020-11-01 15:20:16 -0300

    Updating users migration

--- src/resources/database/migrations/01-users.ts
+++ src/resources/database/migrations/01-users.ts
@@ -3,8 +3,12 @@ import Knex from 'knex'
 export function up(knex: Knex) {
   return knex.schema.createTable('users', table => {
     table.increments('id').primary().notNullable()
-    table.string('username', 120).unique().notNullable()
+    table.string('name', 80).notNullable()
+    table.string('username', 20).unique().notNullable()
+    table.string('email', 120).notNullable()
+    table.string('birthday', 10).notNullable()
     table.string('password').notNullable()
+    table.boolean('privacyTerms').notNullable()
   })
 }
 

598a19977f4133a52578287cbdb1ef13c715fd10
Gabriel-JM <gabriel.josemelo@hotmail.com>
2020-11-01 20:47:52 -0300

    Ensure Login-validator returns the correct response if an email is valid or not

--- src/utils/login-validator.spec.ts
+++ src/utils/login-validator.spec.ts
@@ -19,15 +19,42 @@ describe('Login Validator', () => {
 
   it('should return false if passed an invalid date', () => {
     const sut = makeSut()
-    const validDates = [
+    const invalidDates = [
       '1920-01-00',
       '1999-00-05',
       '2012-02-30',
       '2020-01-02'
     ]
 
-    for(const date of validDates) {
+    for(const date of invalidDates) {
       expect(sut.isDate(date)).toBe(false)
+    }
+  })
+
+  it('should return true if passed a valid email', () => {
+    const sut = makeSut()
+    const validEmails = [
+      'p.a1@email.com',
+      'person@google.com',
+      'ttt@mail.com'
+    ]
+
+    for(const email of validEmails) {
+      expect(sut.isEmail(email)).toBe(true)
+    }
+  })
+
+  it('should return false if passed an invalid email', () => {
+    const sut = makeSut()
+    const invalidEmails = [
+      'p.a1_email.com',
+      'persongoogle.com',
+      'ttt@mail',
+      '@email.com'
+    ]
+
+    for(const email of invalidEmails) {
+      expect(sut.isEmail(email)).toBe(false)
     }
   })
 })

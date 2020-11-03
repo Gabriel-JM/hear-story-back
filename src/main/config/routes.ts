7b8b4851f738a061379eb7aade04a1147cbcfa2d
Gabriel-JM <gabriel.josemelo@hotmail.com>
2020-11-02 21:55:46 -0300

    Adding api to the endpoint

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -2,5 +2,5 @@ import { Application } from 'express'
 import { signupRoutes } from '../routes/signup/signup-routes'
 
 export default (app: Application) => {
-  app.use('/signup', signupRoutes)
+  app.use('/api/signup', signupRoutes)
 }

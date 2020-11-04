2e6ce1d8cc7b632a48c9072224d1f11a6da77662
Gabriel-JM <gabriel.josemelo@hotmail.com>
2020-11-03 23:48:28 -0300

    Configuring cors

--- src/main/config/setup.ts
+++ src/main/config/setup.ts
@@ -1,8 +1,9 @@
 import express, { Application } from 'express'
-import dotenv from 'dotenv-safe'
+import cors from 'cors'
 
 export default (app: Application) => {
-  dotenv.config()
-
   app.use(express.json())
+  app.use(cors({
+    origin: 'http://localhost:3000'
+  }))
 }

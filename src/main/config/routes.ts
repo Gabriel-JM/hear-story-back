579debd5509a5e196834e0128c94bf6215f5ab37
Gabriel-JM <gabriel.josemelo@hotmail.com>
2020-11-29 10:47:57 -0300

    Added categories routes and update login routes

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -1,8 +1,10 @@
 import { Application } from 'express'
+import { categoriesRoutes } from '../routes/categories/categories-routes'
 import { loginRoutes } from '../routes/login/login-routes'
 import { signupRoutes } from '../routes/signup/signup-routes'
 
 export default (app: Application) => {
   app.use('/api/register', signupRoutes)
   app.use('/api/login', loginRoutes)
+  app.use('/api/categories', categoriesRoutes)
 }
